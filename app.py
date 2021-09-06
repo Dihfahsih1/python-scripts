6bb130cd8fc78c3100ef77a8ef59bcb6d7a1f907
Dihfahsih1 <dihfahsihm@gmail.com>
2021-09-06 12:02:00 +0300

    testing the schedule after removing the web dyno

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 # operations@begos.biz, Ibra, Kaima.+25678749058
 
 sched = BlockingScheduler()
-@sched.scheduled_job('cron', day_of_week='mon-fri', hour=11, minute=50)
+@sched.scheduled_job('cron', day_of_week='mon-fri', hour=11, minute=10)
 def mass_mailing_script():
     email_user = "ict@rcsconsult.net"
     password = "pythonista"
