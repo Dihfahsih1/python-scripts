0e5e582c799cd05c3f0bcb2ea00228b039d3fdb7
Dihfahsih1 <dihfahsihm@gmail.com>
2022-01-18 15:48:55 +0300

    commas were causing this bug but fixed it by cleaning the excel

--- excel_computations.py
+++ excel_computations.py
@@ -5,14 +5,27 @@ infile = load_workbook("products.xlsx")
 product_list=infile["Table 1"]
 
 products_per_supplier = {}
+total_value_per_suplier = {}
 
 for product_row in range(2, product_list.max_row + 1):
   supplier_name=product_list.cell(product_row, 4).value
+  inventory = product_list.cell(product_row, 2).value
+  price = product_list.cell(product_row, 3).value
   
-  #calculation for products per supplier
+  #calculation number of products per supplier
   if supplier_name in products_per_supplier:
-    current_nu_products = products_per_supplier[supplier_name] 
-    products_per_supplier[supplier_name] = current_nu_products + 1
+    current_num_products = products_per_supplier.get(supplier_name)
+    products_per_supplier[supplier_name] = current_num_products + 1
   else:
     products_per_supplier[supplier_name] = 1  
-print(products_per_supplier) 
+    
+  #calcualtion number of products per supplier
+  if supplier_name in total_value_per_suplier:
+      current_total_value = total_value_per_suplier.get(supplier_name)
+      total_value_per_suplier[supplier_name] = current_total_value + inventory * price 
+      
+  else:
+    total_value_per_suplier[supplier_name] = inventory * price
+
+print(products_per_supplier)    
+print(total_value_per_suplier)
