dba0263668c64cc5925eb29b92c1577661cab34e
Dihfahsih1 <dihfahsihm@gmail.com>
2022-02-22 23:54:29 +0300

    am really lacking python basics

--- generate_groups_of_3_randomly.py
+++ generate_groups_of_3_randomly.py
@@ -3,14 +3,14 @@ import  csv
 import pandas as pd
 group=1
 membersInGroup=3
-# df = pd.read_csv("class_list.csv") #you can define if there is a header or not in the file
-# participants= df.values.tolist()
+df = pd.read_csv("class_list.csv") #you can define if there is a header or not in the file
+participants= df.values.tolist()
 
 
 # with open('class_list.csv', 'r') as csvfile:
 #   data = csv.reader(csvfile)
 #   participants = list(data)
-participants=["Mugoya Dihfahsih", "Musa Rahim", "David Tugume", "Robert Masajjage", "Fahad Guma", "Henry Mugume","Denis Mbabazi", "Florence Nanteza", "Mwanje Felix", "Gerald Masaba" ,"Isaac King Nsengiyunva"]
+# participants=["Mugoya Dihfahsih", "Musa Rahim", "David Tugume", "Robert Masajjage", "Fahad Guma", "Henry Mugume","Denis Mbabazi", "Florence Nanteza", "Mwanje Felix", "Gerald Masaba" ,"Isaac King Nsengiyunva"]
 print(participants)
 for participant in participants[:]:               
     if membersInGroup==3:
@@ -20,4 +20,4 @@ for participant in participants[:]:
     person=random.choice(participants)
     print(person)
     membersInGroup+=1
-    participants.remove(str(person))
+    participants.append(str(person))
